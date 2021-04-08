0c98fb0387fdd1deb5f0de7b56d5838cecfe0b2b
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-04-08 22:45:27 +0800

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -6,6 +6,6 @@ OLD_IFS=$IFS
 IFS=$(echo -ne "\n\b")
 #-----------------------------------------------------------------------
 #<程序運行-转移压缩包>
-rclone move ${DOWNFILE} Onedrive:/ -P -q --transfers=1 --cache-chunk-size 16M --ignore-errors --no-traverse --create-empty-src-dirs --delete-empty-src-dirs --config "${RCLONE}"
+rclone move ${DOWNFILE} Onedrive:/ -P -q --transfers=1 --ignore-errors --no-traverse --create-empty-src-dirs --delete-empty-src-dirs --config "${RCLONE}"
 IFS=$OLD_IFS
 exit 0
